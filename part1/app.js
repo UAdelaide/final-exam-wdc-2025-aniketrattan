0e917bc423bc1fc9fce16b61bfe1991bbdc60085
Aniket Rattan <aniketrattan2005@gmail.com>
2025-06-20 01:55:55 +0000

    [automatic commit] 2025-06-20 01:55:55 UTC

--- part1/app.js
+++ part1/app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')));;
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let db;
 
