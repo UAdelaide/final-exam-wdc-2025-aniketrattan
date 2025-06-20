9beec47794f319edcbcc077a8e9ab18fbf1f6e91
Aniket Rattan <aniketrattan2005@gmail.com>
2025-06-20 20:08:55 +0000

    [automatic commit] 2025-06-20 20:08:55 UTC

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,34 @@ require('dotenv').config();
 const session = require('express-session');
 const app = express();
 
+let db;
+
+; (async () => {
+    try {
+        // 1a) run your schema file (only if you still need to auto-create/tidy your DB)
+        const setup = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            multipleStatements: true
+        });
+        const ddl = fs.readFileSync(path.join(__dirname, 'dogwalks.sql'), 'utf8');
+        await setup.query(ddl);
+        await setup.end();
+
+        // 1b) open your actual DogWalkService database
+        db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'DogWalkService'
+        });
+    } catch (err) {
+        console.error('DB init failed', err);
+        process.exit(1);
+    }
+})();
+
 // Middleware
 app.use(express.json());
 app.use(session({
