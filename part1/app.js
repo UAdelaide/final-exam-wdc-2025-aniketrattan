4698772b1b6c8751bfb2e438c22b28a408758a98
Aniket Rattan <aniketrattan2005@gmail.com>
2025-06-20 20:27:26 +0000

    [automatic commit] 2025-06-20 20:27:25 UTC

--- part1/app.js
+++ part1/app.js
@@ -95,5 +95,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     }
 });
 
+module.exports = app;
+if (require.main === module) {
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
