4b6cce258d257d9e67e557cae257347ea79d5a40
Aniket Rattan <aniketrattan2005@gmail.com>
2025-06-20 01:32:19 +0000

    [automatic commit] 2025-06-20 01:32:19 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const mysql   = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// 1) Configure your DB connection pool
+// Configure  DB connection pool
 const db = mysql.createPool({
-  host:     'localhost',
-  user:     'YOUR_DB_USER',
-  password: 'YOUR_DB_PASS',
-  database: 'DogWalkService'
+    host: 'localhost',
+    user: 'YOUR_DB_USER',
+    password: 'YOUR_DB_PASS',
+    database: 'DogWalkService'
 });
 
-// 2) (Later) we’ll mount our /api/dogs route here
-
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${PORT}`);
 });
